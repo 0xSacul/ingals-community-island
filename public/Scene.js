277ecd68f0c3b85f0ca6c3123169c1058d73c048
0xSacul <63975424+0xSacul@users.noreply.github.com>
2023-07-07 14:48:07 +0200

    Test Map

---
+++ public/Scene.js
@@ -0,0 +1,12 @@
+class ExternalScene extends window.BaseScene {
+    constructor() {
+        super({
+            player: {
+                spawn: {
+                    x: 61,
+                    y: 9,
+                },
+            },
+        });
+    }
+}

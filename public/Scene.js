d6ccd5226d893ce0aef4d3f31f0c907cd3c8b9fc
0xSacul <63975424+0xSacul@users.noreply.github.com>
2023-07-07 18:07:41 +0200

    Map Update

--- public/Scene.js
+++ public/Scene.js
@@ -1,10 +1,13 @@
 class ExternalScene extends window.BaseScene {
     constructor() {
         super({
+            map: {
+                tileset: "https://sacul.cloud/tileset.png",
+            },
             player: {
                 spawn: {
-                    x: 61,
-                    y: 9,
+                    x: 151,
+                    y: 977,
                 },
             },
         });
